05b31b47b4f27f417e01027516d96f4589b12567
mcibique <mcibique@hotmail.com>
2016-11-02 21:02:47 +0000

    Added redux logger.

--- src/store/dev/store.config.js
+++ src/store/dev/store.config.js
@@ -1,9 +1,17 @@
 import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
 
 import reducers from '../../reducers';
 import DevTools from './store.dev-tools';
 
+let logger = createLogger({
+  actionTransformer: action => ({
+    ...action,
+    type: String(action.type)
+  })
+});
+
 /* @ngInject */
 export default function appConfig($ngReduxProvider) {
-  $ngReduxProvider.createStoreWith(reducers, [thunk], [DevTools.instrument()]);
+  $ngReduxProvider.createStoreWith(reducers, [thunk, logger], [DevTools.instrument()]);
 }
